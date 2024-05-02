cd18b0383a1d64e8cbba39f9a53358954e372bff
admin <admin@DESKTOP-6RDVQOR>
2024-05-03 08:57:39 +0900

    web

---
+++ myhome/src/main/webapp/js/calendar.js
@@ -0,0 +1,67 @@
+/*
+* calendar.js
+*/
+document.addEventListener("DOMContentLoaded", initForm);
+
+function initForm() {
+	let show = document.querySelector('#show');
+	show.appendChild(svc.makeTable());
+	document.querySelector('#show>table').appendChild(svc.makeHeader2());
+	document.querySelector('#show>table').appendChild(svc.makeBody());
+}
+
+const svc = {
+	makeTable: function() {
+		let tbl = document.createElement('table');
+		tbl.setAttribute('border', "2");
+		return tbl;
+	},
+	makeHeader: function() {
+		const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'];
+		let thd = document.createElement('thead');
+		let tr = document.createElement('tr');
+		days.forEach((day) => {
+			let th = document.createElement('th');
+			th.innerHTML = day;
+			tr.appendChild(th);
+		});
+		thd.appendChild(tr);
+		return thd;
+	},
+	makeHeader2: function() {
+		const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'];
+		let tr = days.reduce((acc, curVal) => {
+			let th = document.createElement('th');
+			th.innerHTML = curVal;
+			acc.appendChild(th);
+			return acc;
+		}, document.createElement('tr')); // <tr><th/>*7</tr>
+		let thd = document.createElement('thead');
+		thd.appendChild(tr);
+
+		return thd;
+	},
+	makeBody: function() {
+		let tbd = document.createElement('tbody');
+		let tr = document.createElement('tr');
+		let spaces = 1;
+		for (let i = 0; i < spaces; i++) {
+			let td = document.createElement('td');
+			td.innerHTML = " ";
+			tr.appendChild(td);
+		}
+		// 날짜 출력해주는 부분.
+		for (let d = 1; d <= 30; d++) {
+			let td = document.createElement('td');
+			td.innerHTML = d;
+			tr.appendChild(td);
+			if ((d + spaces) % 7 == 0) {
+				// 토요일이 끝나면 새로운 한주의 시작.
+				tbd.appendChild(tr); // <tr><td/>*7</tr>
+				tr = document.createElement('tr');
+			}
+		}
+		tbd.appendChild(tr);
+		return tbd;
+	}
+} // end of svc.
