ba8189f344ba1411607da6871b5bf698b2cc2aa8
linuxie <lin@think.com>
2024-06-25 12:32:26 +0900

    tt

--- T20240625/src/main/java/co/yedam/book/service/BookService.java
+++ T20240625/src/main/java/co/yedam/book/service/BookService.java
@@ -1,5 +1,15 @@
 package co.yedam.book.service;
 
+import java.util.List;
+
+import co.yedam.book.vo.BookVO;
+
 public interface BookService {
 	String cheeringMessage();
+	// 목록
+	List<BookVO> bookList();
+	// 추가
+	boolean addBook(BookVO bvo);
+	// 삭제
+	boolean removeBook(String bcode);	
 }
